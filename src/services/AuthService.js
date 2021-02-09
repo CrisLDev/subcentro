8ecd05ad4587b366387869fb1aff9764393a2222
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-02-08 22:57:55 -0500

    Agregadas variables de entorno

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const API = 'http://localhost:4123/api/auth';
+const API = process.env.API_URL || 'http://localhost:4123/api';
 
 export const createUser = async (dataToSend) => {
-    return await axios.post(`${API}/register`, dataToSend);
+    return await axios.post(`${API}/auth/register`, dataToSend);
 }
 
 export const loginUser = async (dataToSend) => {
-    return await axios.post(`${API}/login`, dataToSend)
+    return await axios.post(`${API}/auth/login`, dataToSend)
 }
 
 export const reloginUserInReload = async () => {
-    return await axios.get(`${API}/me`);
+    return await axios.get(`${API}/auth/me`);
 }
