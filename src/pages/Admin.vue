99cc46c0c9f211dda12baa3cce1880b11cf27159
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-02-28 22:47:06 -0500

    Ahora se utiliza el v-calendar de viutify y casi podemos mostrar las consultas en el calendario solo pasa que no se ven al principio

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -33,6 +33,67 @@
                 </v-card>
             </v-col>
             <v-col cols="12" class="mt-6 mb-16">
+                <v-sheet
+                    tile
+                    height="54"
+                    class="d-flex"
+                    >
+                    <v-btn
+                        icon
+                        class="ma-2"
+                        @click="$refs.calendar.prev()"
+                    >
+                        <v-icon>mdi-chevron-left</v-icon>
+                    </v-btn>
+                    <v-select
+                        v-model="type"
+                        :items="types"
+                        dense
+                        outlined
+                        hide-details
+                        class="ma-2"
+                        label="type"
+                    ></v-select>
+                    <v-select
+                        v-model="mode"
+                        :items="modes"
+                        dense
+                        outlined
+                        hide-details
+                        label="event-overlap-mode"
+                        class="ma-2"
+                    ></v-select>
+                    <v-select
+                        v-model="weekday"
+                        :items="weekdays"
+                        dense
+                        outlined
+                        hide-details
+                        label="weekdays"
+                        class="ma-2"
+                    ></v-select>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        icon
+                        class="ma-2"
+                        @click="$refs.calendar.next()"
+                    >
+                        <v-icon>mdi-chevron-right</v-icon>
+                    </v-btn>
+                    </v-sheet>
+                    <v-sheet height="600">
+                    <v-calendar
+                        ref="calendar"
+                        v-model="value"
+                        :weekdays="weekday"
+                        :type="type"
+                        :events="events"
+                        :event-overlap-mode="mode"
+                        :event-overlap-threshold="30"
+                        :event-color="getEventColor"
+                        @change="getEvents"
+                    ></v-calendar>
+                    </v-sheet>
             </v-col>
         </v-row>
     </v-container>
@@ -45,19 +106,63 @@ export default {
     name: "Admin",
     data: () => ({
       reveal: false,
+       type: 'month',
+      types: ['month', 'week', 'day', '4day'],
+      mode: 'stack',
+      modes: ['stack', 'column'],
+      weekday: [0, 1, 2, 3, 4, 5, 6],
+      weekdays: [
+        { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
+        { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
+        { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
+        { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
+      ],
+      value: '',
+      events: [],
+      colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+      names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
     }),
     components: {
         AdminUserEdit,
     },
     computed: {...mapGetters(["usersInBd"])},
     methods: {
-        ...mapActions(["getusersFromBD","deleteUser"]),
+        ...mapActions(["getusersFromBD","deleteUser","getDatesFromBD"]),
         deleteU(id){
             this.deleteUser(id)
-        }
+        },
+        getEvents () {
+        const events = []
+
+        //const min = new Date(`${start.date}T00:00:00`)
+        //const max = new Date(`${end.date}T23:59:59`)
+        //const days = (max.getTime() - min.getTime()) / 86400000
+        //const eventCount = this.rnd(days, days + 20)
+        const dates = this.$store.getters.datesInBd;
+        //const eventCount = dates;
+
+        Object.keys(dates).map((evento) => 
+        {const timestamp = evento.date + ' ' + evento.hour;
+        events.push({
+            name: 'Consulta',
+            start: timestamp,
+            end:timestamp,
+            color: this.colors[this.rnd(0, this.colors.length - 1)]
+          })}
+          );
+
+        this.events = events
+      },
+      getEventColor (event) {
+        return event.color
+      },
+      rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+      },
     },
     created() {
         this.getusersFromBD();
+        this.getDatesFromBD();
     },
 }
 </script>
